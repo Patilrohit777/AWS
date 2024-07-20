922f32b7355fce51bcbaaa8805b1bba28351eee8
Rohit Patil <patilr566@gmail.com>
2024-07-20 12:03:12 +0530

    docker installation steps added on linux server

---
+++ docker_installation_steps.sh
@@ -0,0 +1,20 @@
+sudo yum update -y
+
+sudo yum install docker -y
+
+docker --version
+
+sudo systemctl start docker
+
+sudo -i
+
+yum update
+
+yum --version
+
+docker run hello-world
+
+
+
+which docker
+
